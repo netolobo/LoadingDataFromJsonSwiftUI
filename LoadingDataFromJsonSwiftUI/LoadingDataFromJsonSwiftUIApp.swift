9eb967fd2d73b9594b279c597b484b3cd4079d72
netolobo <desilio@gmail.com>
2023-12-03 09:04:14 -0800

    Initial Commit

---
+++ LoadingDataFromJsonSwiftUI/LoadingDataFromJsonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingDataFromJsonSwiftUIApp.swift
+//  LoadingDataFromJsonSwiftUI
+//
+//  Created by Neto Lobo on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LoadingDataFromJsonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
